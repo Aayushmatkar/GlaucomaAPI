eb735ea5e2de80a3c5df709b0178f7d50300f2ae
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-02-21 23:50:58 -0500

    Add files via upload

---
+++ test_main_2.py
@@ -0,0 +1,49 @@
+ #mainurl.py
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import tensorflow as tf
+from keras.preprocessing import image
+import numpy as np
+
+app = FastAPI()
+
+# Load the saved model
+#model = tf.keras.models.load_model('csa_glaucoma1.h5')
+
+# Define the request payload using Pydantic BaseModel
+class ImagePayload(BaseModel):
+    image_url: str
+
+
+@app.get("/")
+def foobar():
+    return {
+        "foo":"bar"
+    }
+
+
+# Endpoint to make predictions
+@app.get("/predict")
+async def predict_image():
+        
+        # Load the saved model
+        model = tf.keras.models.load_model('csa_glaucoma1.h5')
+    
+        pic = r"C:\Users\aayus\OneDrive\Desktop\WINTER 2024\project\val\glaucoma false.png"
+        # Download and preprocess the image
+        img = image.load_img(pic, target_size=(150, 150))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0  # Normalize pixel values to [0, 1]
+
+        # Make predictions using te loaded model
+        prediction = model.predict(img_array)
+        result = True if prediction[0][0] > 0.5 else False 
+        # print(prediction)
+        return {"Glaucoma": result,
+                "value": float(prediction[0][0])}
+        #testing api
+        # return {"test":"Hello World!",
+                # "Data": test_var
+                # }
