5eb6affead31016e2cdd6205679ddf25b911564e
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-02-22 15:07:47 -0500

    Update test_main_2.py

--- test_main_2.py
+++ test_main_2.py
@@ -37,7 +37,7 @@ async def predict_image():
         # Load the saved model
         model = tf.keras.models.load_model('csa_glaucoma1.h5')
     
-        pic = r"C:\Users\aayus\OneDrive\Desktop\WINTER 2024\project\val\glaucoma false.png"
+        pic = r"glaucoma false.png"
         # Download and preprocess the image
         img = image.load_img(pic, target_size=(150, 150))
         img_array = image.img_to_array(img)
