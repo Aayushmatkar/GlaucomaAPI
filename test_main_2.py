0e1193d5b22a906fcaf2d8dcc6ba2fc88aa74c1d
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-04-03 14:40:59 -0400

    Update test_main_2.py

--- test_main_2.py
+++ test_main_2.py
@@ -1,56 +1,34 @@
- #mainurl.py
-
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import tensorflow as tf
-from keras.preprocessing import image
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from PIL import Image
 import numpy as np
-import requests
+from io import BytesIO
+import tensorflow as tf
 
 app = FastAPI()
 
 # Load the saved model
-#model = tf.keras.models.load_model('csa_glaucoma1.h5')
+model = tf.keras.models.load_model('small_model_pipeline.pkl')
 
-# Define the request payload using Pydantic BaseModel
-# class ImagePayload(BaseModel):
-#     image_url: str
+@app.post("/predict")
+async def predict_image(file: UploadFile = File(...)):
+    # Check if the uploaded file is an image
+    if file.content_type.startswith('image/') is False:
+        raise HTTPException(status_code=415, detail="Unsupported media type")
 
+    # Read the image file
+    contents = await file.read()
+    image = Image.open(BytesIO(contents))
 
-@app.get("/")
-def foobar():
+    # Preprocess the image
+    image = image.resize((150, 150))  # Resize to match model input size
+    img_array = np.array(image) / 255.0  # Convert to numpy array and normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+    # Make predictions using the loaded model
+    prediction = model.predict(img_array)
+    result = True if prediction[0][0] > 0.5 else False
+
     return {
-        "foo":"bar"
+        "Glaucoma": result,
+        "Confidence": float(prediction[0][0])
     }
-# Endpoint to make predictions0
-# import requests
-
-# url = "https://glaucoma-detection-api-52f531d65dd6.herokuapp.com/predict"
-# files = {"file": open("glauc True 1.png", 'rb')}
-# res = requests.post(url, files=files)
-
-
-# Endpoint to make predictions
-@app.get("/")
-async def predict_image():
-        
-        # Load the saved model
-        model = tf.keras.models.load_model('/app/csa_glaucoma1.h5')
-    
-        pic = "/app/glaucoma false.png"
-        # Download and preprocess the image
-        img = image.load_img(pic, target_size=(150, 150))
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0  # Normalize pixel values to [0, 1]
-
-        # Make predictions using te loaded model
-        prediction = model.predict(img_array)
-        result = True if prediction[0][0] > 0.5 else False 
-        # print(prediction)
-        return {"Glaucoma": result,
-                "value": float(prediction[0][0])}
-        #testing api
-        # return {"test":"Hello World!",
-                # "Data": test_var
-                # }
